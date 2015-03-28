40cdd96732ed33088d946606a1ba083d044b5367
Björn Fischer <privat@bjorn-fischer.de>
2015-03-28 16:20:17 +0100

    Initial design for landing page

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,43 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files
+          'dist/css/style.css': 'src/sass/main.scss'        // 'destination': 'source'
+        }
+      }
+    },
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'src/mockup',
+        src: ['*'],
+        dest: 'dist/mockup/',
+      },
+    },
+    watch: {
+      main: {
+        files: ['src/**/*'],
+        tasks: ['build']
+      },
+      livereload: {
+        options: {livereload: true},
+        files: ['dist/**/*']
+      }
+    }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['sass', 'copy']);
+
   // Default task(s).
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['build', 'watch']);
 
 };
--- README.md
+++ README.md
@@ -7,4 +7,6 @@ Repository for a university project based on PHP.
 
 Requires node npm and grunt-cli ```npm install -g grunt-cli```
 
+Requires ruby and sass. `sass` should be available via command line.
+
 Run ```grunt``` to build
