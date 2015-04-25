fc24c0a006155bf048b54ca82e073427dd4fc6c7
fischerman <privat@bjorn-fischer.de>
2015-04-25 17:26:22 +0200

    Change error level to throw database exceptions

--- src/app/controller/NewIdeaController.php
+++ src/app/controller/NewIdeaController.php
@@ -2,16 +2,18 @@
 class NewIdeaController extends Controller {
 	public function incomingRequest(&$req, &$data, Response $res) {
 		if($_SERVER['REQUEST_METHOD'] == 'POST') {
-			$db = $req['db'];
-			$stmt = $db->prepare("INSERT INTO ideas (ideaName, description, createdBy, createdAt) VALUES (:name, :description, :creator, now())");
-			$stmt->bindParam(':name', $_POST["name"]);
-			$stmt->bindParam(':description', $_POST["description"]);
-			$stmt->bindParam(':creator', $_SESSION["userId"]);
-			$stmt->execute();
-			$row = $stmt->fetch();
-			$id = $db->lastInsertId();
-
-			header('Location: /idea/' . $id);
+			try {
+				$db = $req['db'];
+				$stmt = $db->prepare("INSERT INTO ideas (ideaName, description, createdBy, createdAt) VALUES (:name, :description, :creator, now())");
+				$stmt->bindParam(':name', $_POST["name"]);
+				$stmt->bindParam(':description', $_POST["description"]);
+				$stmt->bindParam(':creator', $_SESSION["userId"]);
+				$stmt->execute();
+				$id = $db->lastInsertId();
+			} catch(PDOException $e) {
+				die("Failed to create idea");
+			}
+			header('Location: /idea/' . $id . '/');
 			die();
 		} else {
 			// else just return the creation 
--- src/app/middleware/DatabaseMiddleware.php
+++ src/app/middleware/DatabaseMiddleware.php
@@ -3,6 +3,7 @@ class DatabaseMiddleware extends Middleware {
 	public function incomingRequest(&$req) {
 		try {
 			$req["db"] = new PDO(constant("DB_DSN"), constant("DB_USER"));
+			$req["db"]->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch(PDOException $e) {
 			die('Failed to connect to DB');
 		}
