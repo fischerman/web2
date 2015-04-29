2957ebf6df75662d899b39b7de24ad5c2183d2fe
fischerman <privat@bjorn-fischer.de>
2015-04-29 22:30:09 +0200

    Implement db password constant

--- src/app/middleware/DatabaseMiddleware.php
+++ src/app/middleware/DatabaseMiddleware.php
@@ -2,7 +2,7 @@
 class DatabaseMiddleware extends Middleware {
 	public function incomingRequest(&$req) {
 		try {
-			$req["db"] = new PDO(constant("DB_DSN"), constant("DB_USER"));
+			$req["db"] = new PDO(constant("DB_DSN"), constant("DB_USER"), constant("DB_PASS"));
 			$req["db"]->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		} catch(PDOException $e) {
 			die('Failed to connect to DB');
